9e45586b10b2115c6a22867823fb9eb7690f7c84
Petrov-connect <Petrov.connect@gmail.com>
2020-12-29 22:44:04 +0200

    java code

--- AreaOfFigures.java
+++ AreaOfFigures.java
@@ -4,20 +4,29 @@ public class AreaOfFigures {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String figure = scan.nextLine();
-        if (figure.equals("square")) {
-            double a = Double.parseDouble(scan.nextLine());
-            System.out.printf("%.3f", (a * a));
-        } else if (figure.equals("rectangle")) {
-            double a = Double.parseDouble(scan.nextLine());
-            double b = Double.parseDouble(scan.nextLine());
-            System.out.printf("%.3f", (a * b));
-        } else if (figure.equals("circle")) {
-            double a = Double.parseDouble(scan.nextLine());
-            System.out.printf("%.3f", (Math.PI * a * a));
-        } else if (figure.equals("triangle")) {
-            double a = Double.parseDouble(scan.nextLine());
-            double b = Double.parseDouble(scan.nextLine());
-            System.out.printf("%.3f", (a * b / 2));
+        switch (figure) {
+            case "square": {
+                double a = Double.parseDouble(scan.nextLine());
+                System.out.printf("%.3f", (a * a));
+                break;
+            }
+            case "rectangle": {
+                double a = Double.parseDouble(scan.nextLine());
+                double b = Double.parseDouble(scan.nextLine());
+                System.out.printf("%.3f", (a * b));
+                break;
+            }
+            case "circle": {
+                double a = Double.parseDouble(scan.nextLine());
+                System.out.printf("%.3f", (Math.PI * a * a));
+                break;
+            }
+            case "triangle": {
+                double a = Double.parseDouble(scan.nextLine());
+                double b = Double.parseDouble(scan.nextLine());
+                System.out.printf("%.3f", (a * b / 2));
+                break;
+            }
         }
     }
 }
