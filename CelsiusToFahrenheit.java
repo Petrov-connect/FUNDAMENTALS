8799290bbbcc903414c7c259aa6400574afde990
Petrov-connect <Petrov.connect@gmail.com>
2021-01-01 19:41:28 +0200

    java code

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class CelsiusToFahrenheit {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double celsius = Double.parseDouble(scan.nextLine());
-        double result = (celsius * 9/5) + 32;
-        System.out.printf("%.2f",result);
+
+        double result = (celsius * 9 / 5) + 32;
+
+        System.out.printf("%.2f", result);
     }
 }
 
