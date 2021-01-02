fc5b3401c78f3a0bbc63127b8599e3d33bb499dc
Petrov-connect <Petrov.connect@gmail.com>
2021-01-02 20:17:26 +0200

    java code

--- Equal3Numbers.java
+++ Equal3Numbers.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class Equal3Numbers {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        int num1=Integer.parseInt(scan.nextLine());
-        int num2=Integer.parseInt(scan.nextLine());
-        int num3=Integer.parseInt(scan.nextLine());
-        if(num1==num2&&num2==num3){
+        int num1 = Integer.parseInt(scan.nextLine());
+        int num2 = Integer.parseInt(scan.nextLine());
+        int num3 = Integer.parseInt(scan.nextLine());
+
+        if (num1 == num2 && num2 == num3) {
             System.out.println("yes");
-        }else{
+        } else {
             System.out.println("no");
         }
     }
