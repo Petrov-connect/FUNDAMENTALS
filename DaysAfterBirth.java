d1d427fbf5f0c76e7a50dbc3a38e437fb2cb32fe
Petrov-connect <Petrov.connect@gmail.com>
2021-01-02 20:16:46 +0200

    java code

--- DaysAfterBirth.java
+++ DaysAfterBirth.java
@@ -2,20 +2,25 @@ import java.util.Scanner;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.text.ParseException;
+
 // created by J.M.
 public class DaysAfterBirth {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String oldDate = scan.nextLine();
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         Calendar c = Calendar.getInstance();
-        try{
+        try {
             c.setTime(sdf.parse(oldDate));
-        }catch(ParseException e){
+        } catch (ParseException e) {
             e.printStackTrace();
         }
         c.add(Calendar.DAY_OF_MONTH, 999);
         String newDate = sdf.format(c.getTime());
+
         System.out.println(newDate);
     }
 }
