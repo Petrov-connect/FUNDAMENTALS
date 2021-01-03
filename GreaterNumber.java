886f3d40572d6c1394e9e75a439d9edae3f8e77a
Petrov-connect <Petrov.connect@gmail.com>
2021-01-03 18:42:42 +0200

    java code

--- GreaterNumber.java
+++ GreaterNumber.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class GreaterNumber {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int num1 = Integer.parseInt(scan.nextLine());
         int num2 = Integer.parseInt(scan.nextLine());
+
         System.out.println(Math.max(num1, num2));
     }
 }
