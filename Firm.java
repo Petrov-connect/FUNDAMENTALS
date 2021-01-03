df81e14ba282616217823438fc621f954d736e0b
Petrov-connect <Petrov.connect@gmail.com>
2021-01-03 18:42:07 +0200

    java code

--- Firm.java
+++ Firm.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class Firm {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int needHours = Integer.parseInt(scan.nextLine());
         int daysForWork = Integer.parseInt(scan.nextLine());
         int employees = Integer.parseInt(scan.nextLine());
-        double hours = Math.floor(((employees * 2 ) + (8*employees))*daysForWork*0.9);
+
+        double hours = Math.floor(((employees * 2) + (8 * employees)) * daysForWork * 0.9);
+
         if (hours >= needHours) {
             double left = hours - needHours;
             System.out.printf("Yes!%.0f hours left.", left);
