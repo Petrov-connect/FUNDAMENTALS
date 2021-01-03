849cd2611781141dd85a77c11245a1ce5f66eb50
Petrov-connect <Petrov.connect@gmail.com>
2021-01-03 18:41:44 +0200

    java code

--- ExcellentOrNot.java
+++ ExcellentOrNot.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class ExcellentOrNot {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        double evaluation=Double.parseDouble(scan.nextLine());
-        if(evaluation>=5.50){
+        double evaluation = Double.parseDouble(scan.nextLine());
+
+        if (evaluation >= 5.50) {
             System.out.println("Excellent!");
-        }else{
+        } else {
             System.out.println("Not excellent.");
         }
     }
