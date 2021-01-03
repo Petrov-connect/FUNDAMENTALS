f86441866d0ce95500756e8c846c3e87cb9130ae
Petrov-connect <Petrov.connect@gmail.com>
2021-01-03 18:41:29 +0200

    java code

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class EvenOrOdd {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int a = Integer.parseInt(scan.nextLine());
-        if(a%2==0){
+
+        if (a % 2 == 0) {
             System.out.println("even");
-        }
-        else {
+        } else {
             System.out.println("odd");
         }
     }
