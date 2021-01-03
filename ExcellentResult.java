10f322f5678aca79d60f4954621354b7753ef29d
Petrov-connect <Petrov.connect@gmail.com>
2021-01-03 18:41:56 +0200

    java code

--- ExcellentResult.java
+++ ExcellentResult.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class ExcellentResult {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
-        double evaluation=Double.parseDouble(scan.nextLine());
-        if(evaluation>=5.50) {
+        double evaluation = Double.parseDouble(scan.nextLine());
+
+        if (evaluation >= 5.50) {
             System.out.println("Excellent!");
         }
     }
