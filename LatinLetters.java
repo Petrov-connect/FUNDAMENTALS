d6c9f3543c2c6e0cc274bf1ecba7b42be948c558
Petrov-connect <Petrov.connect@gmail.com>
2021-01-04 18:06:56 +0200

    java code

--- LatinLetters.java
+++ LatinLetters.java
@@ -1,8 +1,12 @@
+//created by J.M.
 public class LatinLetters {
+
     public static void main(String[] args) {
+
         System.out.print("Latin alphabet: ");
+
         for (char i = 'a'; i <= 'z'; i++) {
-            System.out.print(" "+i);
+            System.out.print(" " + i);
         }
         System.out.println();
     }
