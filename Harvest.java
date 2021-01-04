e70edd366a1607f217423f325e88ca3684a2f9e9
Petrov-connect <Petrov.connect@gmail.com>
2021-01-04 18:06:08 +0200

    java code

--- Harvest.java
+++ Harvest.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class Harvest {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int x = Integer.parseInt(scan.nextLine());
         double y = Double.parseDouble(scan.nextLine());
         int z = Integer.parseInt(scan.nextLine());
         int workers = Integer.parseInt(scan.nextLine());
+
         double allKg = x * y;
         double liters = (allKg * 0.4) / 2.5;
+
         if (liters >= z) {
             double left = (liters - z);
             double perOne = (left / workers);
