b49dd5e28682241038d53899f1ec89d6532748c9
Petrov-connect <Petrov.connect@gmail.com>
2021-01-04 18:06:34 +0200

    java code

--- InchesToCentimeters.java
+++ InchesToCentimeters.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class InchesToCentimeters {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("Inches = ");
         double inches = Double.parseDouble(scan.nextLine());
-        System.out.printf("Centimeters = %f",inches*2.54);
+
+        System.out.printf("Centimeters = %f", inches * 2.54);
     }
 }
