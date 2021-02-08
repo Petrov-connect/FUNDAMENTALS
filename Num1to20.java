4c7820ce3f565e2ac9a7f163695bebce1fef86a7
Petrov-connect <Petrov.connect@gmail.com>
2021-02-08 13:04:19 +0200

    java code

--- Num1to20.java
+++ Num1to20.java
@@ -1,7 +1,10 @@
 // created by J.M.
 public class Num1to20 {
+
     public static void main(String[] args) {
-        for (int a = 1; a <= 20; a++)
-            { System.out.println(a); }
+
+        for (int a = 1; a <= 20; a++) {
+            System.out.println(a);
+        }
     }
 }
