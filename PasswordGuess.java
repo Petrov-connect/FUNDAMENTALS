cacb0e9602ca2179ea6e90dabad3721725d0073d
Petrov-connect <Petrov.connect@gmail.com>
2021-02-11 13:16:16 +0200

    java code

--- PasswordGuess.java
+++ PasswordGuess.java
@@ -1,13 +1,16 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class PasswordGuess {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         String password = scan.nextLine();
+
         if (password.equals("s3cr3t!P@ssw0rd")) {
             System.out.println("Welcome");
-        }
-        else {
+        } else {
             System.out.println("Wrong password!");
         }
     }
