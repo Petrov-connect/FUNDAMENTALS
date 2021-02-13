77b6950c882b63afabd28e5c48f1a9f70da49ef6
Petrov-connect <Petrov.connect@gmail.com>
2021-02-13 22:10:32 +0200

    java code

--- SquareArea.java
+++ SquareArea.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class SquareArea {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         System.out.print("a= ");
-        int a=Integer.parseInt(scan.nextLine());
-        System.out.printf("Square area = %d",a*a);
+        int a = Integer.parseInt(scan.nextLine());
+
+        System.out.printf("Square area = %d", a * a);
     }
 }
