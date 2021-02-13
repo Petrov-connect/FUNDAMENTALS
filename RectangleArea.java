73610a0e7ec8793a71d1f8f5c7b57b2ef01dd8de
Petrov-connect <Petrov.connect@gmail.com>
2021-02-13 22:09:39 +0200

    java code

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class RectangleArea {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int a = Integer.parseInt(scan.nextLine());
         int b = Integer.parseInt(scan.nextLine());
+
         int area = a * b;
+
         System.out.println(area);
     }
 }
