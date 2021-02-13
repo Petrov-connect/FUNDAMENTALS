7ee4504a01586e83da0100dd314c3a2acf598bba
Petrov-connect <Petrov.connect@gmail.com>
2021-02-13 22:10:42 +0200

    java code

--- SquareOfStars.java
+++ SquareOfStars.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class SquareOfStars {
+
     public static void main(String[] args) {
+
         Scanner console = new Scanner(System.in);
         int n = Integer.parseInt(console.nextLine());
-        for (int i=1 ; i<=n; i++){
-            for (int j=1;j<=n;j++){
-                if (i==1 || i==n || j==1 || j==n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
                     System.out.print("*");
-                }
-                else{
+                } else {
                     System.out.print(" ");
                 }
             }
