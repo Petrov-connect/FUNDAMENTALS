191fad33a673a4b3b58b796f671d5e423bd8011a
Petrov-connect <Petrov.connect@gmail.com>
2021-02-13 22:09:49 +0200

    java code

--- RectangleArea2.java
+++ RectangleArea2.java
@@ -1,16 +1,21 @@
 import java.text.DecimalFormat;
 import java.util.Scanner;
+
 // created by J.M.
 public class RectangleArea2 {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double x1 = Double.parseDouble(scan.nextLine());
         double y1 = Double.parseDouble(scan.nextLine());
         double x2 = Double.parseDouble(scan.nextLine());
         double y2 = Double.parseDouble(scan.nextLine());
+
         double width = Math.max(x1, x2) - Math.min(x1, x2);
         double height = Math.max(y1, y2) - Math.min(y1, y2);
         DecimalFormat decimalFormat = new DecimalFormat("#.#########");
+
         System.out.println(decimalFormat.format(width * height));
         System.out.println(decimalFormat.format(2 * (width + height)));
     }
