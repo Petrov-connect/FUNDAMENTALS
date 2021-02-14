931840f7f0ad5babcf86a835341004114ea08b6e
Petrov-connect <Petrov.connect@gmail.com>
2021-02-14 21:13:24 +0200

    java code

--- TimePlusMinutes.java
+++ TimePlusMinutes.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class TimePlusMinutes {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int hours = Integer.parseInt(scan.nextLine());
         int minutes = Integer.parseInt(scan.nextLine());
+
         int totalMinutes = hours * 60 + minutes + 15;
         int h = totalMinutes / 60;
         int m = totalMinutes % 60;
+
         if (h >= 24) {
             h -= 24;
         }
