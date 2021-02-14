b012553fa555a71f1de88701d36580180caa624c
Petrov-connect <Petrov.connect@gmail.com>
2021-02-14 21:13:14 +0200

    java code

--- SumSeconds.java
+++ SumSeconds.java
@@ -1,14 +1,19 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class SumSeconds {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         int time1 = Integer.parseInt(scan.nextLine());
         int time2 = Integer.parseInt(scan.nextLine());
         int time3 = Integer.parseInt(scan.nextLine());
+
         int totalTime = time1 + time2 + time3;
         int minute = totalTime / 60;
         int second = totalTime % 60;
+
         if (second < 10) {
             System.out.printf("%d:%02d", minute, second);
         } else {
