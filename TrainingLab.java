d88393da926a37cdaa398c378fed2fb56f7e393b
Petrov-connect <Petrov.connect@gmail.com>
2021-02-14 21:13:44 +0200

    java code

--- TrainingLab.java
+++ TrainingLab.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class TrainingLab {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double length = Double.parseDouble(scan.nextLine());
         double width = Double.parseDouble(scan.nextLine());
-        System.out.printf("%d",(((int)(((width*100)-100)/70) * (int)((length*100)/120))-3));
+
+        System.out.printf("%d", (((int) (((width * 100) - 100) / 70) * (int) ((length * 100) / 120)) - 3));
     }
 }
