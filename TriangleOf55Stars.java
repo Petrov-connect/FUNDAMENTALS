898f63ffa40dcfe9f9283b7850a1c9268e69bd4b
Petrov-connect <Petrov.connect@gmail.com>
2021-02-15 19:58:44 +0200

    java code

--- TriangleOf55Stars.java
+++ TriangleOf55Stars.java
@@ -1,6 +1,8 @@
 // created by J.M.
 public class TriangleOf55Stars {
+
     public static void main(String[] args) {
+
         for (int x = 1; x <= 10; x++) {
             for (int y = 0; y < x; y++) {
                 System.out.print("*");
