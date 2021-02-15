2c8e79a01398b2ce21017269f164123e989ac503
Petrov-connect <Petrov.connect@gmail.com>
2021-02-15 19:59:16 +0200

    java code

--- VegetableMarket.java
+++ VegetableMarket.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
+
 // created by J.M.
 public class VegetableMarket {
+
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
         double priceOfVege = Double.parseDouble(scan.nextLine());
         double priceOfFruit = Double.parseDouble(scan.nextLine());
         int totalOfVege = Integer.parseInt(scan.nextLine());
         int totalOfFruit = Integer.parseInt(scan.nextLine());
-        System.out.println(((totalOfVege * priceOfVege) + (totalOfFruit * priceOfFruit))/1.94);
+
+        System.out.println(((totalOfVege * priceOfVege) + (totalOfFruit * priceOfFruit)) / 1.94);
     }
 }
